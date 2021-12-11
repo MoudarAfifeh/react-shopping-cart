7f652a153bc654be20626b6bd4fb3c9fd793a7b8
modar <moaf12oo@gmail.com>
2021-12-11 00:14:18 -0800

    fix product name

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -5,7 +5,7 @@ import Modal from 'react-modal';
 import Zoom from 'react-reveal/Zoom';
 import { connect } from 'react-redux';
 import { fetchProducts } from '../actions/productActions';
-
+//master
 class Products extends Component {
     constructor(props) {
         super(props)
