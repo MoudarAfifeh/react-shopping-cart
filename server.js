63c76f9412523ca7b3cda9d7fb08c49f92c8028b
modar <moaf12oo@gmail.com>
2021-12-17 01:39:53 -0800

    create-order

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
-const cors=require("cors");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
- 
- app.use(cors(corsOptions));
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 
 mongoose.connect(
     process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
@@ -52,6 +52,41 @@ app.delete("/api/products/:id", async (req, res) => {
     res.send(deletedProduct);
 });
 
+const Order = mongoose.model("order", new mongoose.Schema({
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    cartItems: [{
+        _id: String,
+        title: String,
+        price: Number,
+        count: Number
+    }]
+},
+    {
+        timestamps: true
+    }
+));
+
+app.post('/api/orders', async (req, res) => {
+    if (
+        !req.body.name ||
+        !req.body.email ||
+        !req.body.address ||
+        !req.body.total ||
+        !req.body.cartItems
+    ) {
+        return res.send({ message: "Data is required." })
+    }
+    const order = await Order(req.body).save();
+    res.send(order);
+})
+
 const port = process.env.PORT || 5000;
-console.log('port',port)
+console.log('port', port)
 app.listen(port, () => console.log("serve at http://localhost:5000"));
